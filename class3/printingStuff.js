77483e5aa906f26f314436fcd696f778f0731c24
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-09-11 08:51:43 -0300

    printing stuff

---
+++ class3/printingStuff.js
@@ -0,0 +1,5 @@
+console.log('I am the new generation "Johnatan"');
+console.log("Oh babe, why do you treat me like that? - 'mc marcinho'");
+console.log(`I'm human after all, don't put your blame on me - 'rag bone"`);
+
+// Three ways of printing stuff ' " `
