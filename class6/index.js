5b0551655b4d11c79052fa32b773a0b0b68921fb
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-09-12 10:23:36 -0300

    learning about how to declare variables

---
+++ class6/index.js
@@ -0,0 +1,8 @@
+// Defining variables with let
+
+let playerName = "Lebron"
+let playerSurname = "James"
+
+console.log(playerName + " " + playerSurname)
+
+// The professor instructed to never use var. It is another way to declare a varible, but isn't commonly used nowadays.
