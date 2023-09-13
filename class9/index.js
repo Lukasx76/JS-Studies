5e7f66fc5e5e17636125e54369df5259584b0056
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-09-13 15:35:28 -0300

    difference between var and let

---
+++ class9/index.js
@@ -0,0 +1,9 @@
+// the main difference of var and let is:
+
+var personName = "Josh";
+var personName = "Drake";
+
+// this is allowed with var but it is not with let
+
+// let personName2 = "Billy";
+// let personName2 = "Joe";
