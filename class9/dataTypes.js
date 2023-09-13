7776a130874d6e2f358704cf0fba5b57cbf48578
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-09-13 15:44:03 -0300

    primitive data types

---
+++ class9/dataTypes.js
@@ -0,0 +1,10 @@
+let firstName = 'Lucas'; // string
+let middleName = "Feitoza"; // string
+let lastName = `Gomes`; // string
+const pi = 3.14; // number
+const brazil5titleDate = 2002; // number
+let randomText; // undefined
+let booleanValue = true; // boolean
+let anotherBooleanValue = false; // boolean
+let voidVariable = null; // null
+const sym = Symbol() // symbol
