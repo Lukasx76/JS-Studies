bf29fad30bbea13a4a6704333b9ab8f909b2b9f5
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-09-14 15:11:45 -0300

    learning about operators

---
+++ class10/javaScriptOperators.js
@@ -0,0 +1,27 @@
+// arithmetic operators
+/**
+ * sum             +
+ * subtraction     -
+ * multiplication  *
+ * division        /
+ * power           **
+ * module          %
+ */
+
+// assignment operators
+
+/**
+ * simple assignment          =
+ * sum assignment             +=
+ * subtraction assignment     -=
+ * division assignment        /= 
+ * power assignment           **=
+ * multiplication assignment  *= 
+ */
+
+// increment operators and decrement
+
+/**
+ * increment number++ or ++number
+ * decrement number-- or --number
+ */
