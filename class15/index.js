1ca907f179cd15c1db4fd6becd2f146489519b15
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-09-16 22:55:07 -0300

    a little a bit of the Math object

---
+++ class15/index.js
@@ -0,0 +1,20 @@
+// learning about the Math object
+
+// you can divide things by zero, this is cray.
+
+let num = 100;
+console.log(num / 0);
+
+// returning min and max values of a set of elements
+
+console.log(Math.min(1,2,3,4,5,6,7,9,999));
+console.log(Math.max(1,2,3,4,5,6,7,9,999));
+
+// rounding 
+
+console.log(Math.round(0.51));
+console.log(Math.round(0.4));
+
+// getting the square root of things
+
+console.log(num ** 0.5)
