586c566fa89daeb3e2aa21be57e64a3879ae6c29
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-09-16 22:46:29 -0300

    learning more about numbers

---
+++ class14/index.js
@@ -0,0 +1,14 @@
+// number methods
+
+let num = 0.9876499;
+let num2 = 2.1;
+
+console.log(num + num2);
+// rounding a number
+
+let sum = num + num2;
+console.log(sum.toFixed(2));
+
+// other checks
+console.log(Number.isInteger(sum));
+console.log(Number.isNaN(num));
