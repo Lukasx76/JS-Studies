89d600b9852015b3d8d8e50b5f10e6aabe7fd432
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-09-17 23:08:23 -0300

    learning about functions

---
+++ class17/functions.js
@@ -0,0 +1,21 @@
+// old school method to define a function
+
+function sum(x, y){
+    return x + y
+};
+
+// enchanced method to define a function
+
+const multiplication = function(x, y){
+    return x * y
+};
+
+// more enchanced method to define a function
+
+const subtraction = (x, y) => {return x - y};
+
+// lazy way to define a function(pure arrow function)
+
+const root = n => n ** 0.5;
+
+
