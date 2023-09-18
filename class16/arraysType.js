60d427f71ad207aa63e561b34216cda89597aa89
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-09-17 22:57:19 -0300

    learning how to use arrays

---
+++ class16/arraysType.js
@@ -0,0 +1,7 @@
+let fruitsArray = ["apple", "orange"]; // defining the array
+
+fruitsArray.push("banana"); // adding to the final
+fruitsArray.unshift("grape"); // adding to the beginning
+fruitsArray.slice(0, fruitsArray.length + 1); // slicing the array
+fruitsArray.pop(); // removing the last element
+fruitsArray.shift(); // removing the first element
