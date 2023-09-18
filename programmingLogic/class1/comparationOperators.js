ce41fd0af55bc62ff2c7c27de41f75a10bc7ad3f
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-09-18 16:20:17 -0300

    comparation operators

---
+++ programmingLogic/class1/comparationOperators.js
@@ -0,0 +1,10 @@
+/**
+ * > greater than
+ * >= greater equal than
+ * <  less than
+ * <= less equal than
+ * == equal(check only the value) ********** not commonly used
+ * === equal(check the value and the type)
+ * !=  not equal(check only the value) ********** not commonly used
+ * !== not equal(check the value and the type)
+ */
