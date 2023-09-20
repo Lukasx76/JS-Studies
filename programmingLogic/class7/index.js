bf38255221d1133305004fdbdc1186202a64c74a
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-09-20 12:55:59 -0300

    learning about dates

---
+++ programmingLogic/class7/index.js
@@ -0,0 +1,15 @@
+// working with the Date object
+
+function getDate(){
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDay();
+    const hour = date.getUTCHours();
+    const minute = date.getMinutes();
+    const second = date.getSeconds();
+
+    return `${day}-${month}-${year} ${hour}:${minute}:${second}`
+}
+
+console.log(getDate())
