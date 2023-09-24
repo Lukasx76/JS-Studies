71c821bc5af15c038a42ceb924517cf42da1e3fe
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-09-24 13:02:15 -0300

    learning about for of

---
+++ programmingLogic/class16/index.js
@@ -0,0 +1,10 @@
+// learning about for of
+
+// this type of for returns only the value of the iterable
+// for in return just the index
+
+const cars = ["dodge challenger", "mustang shelby", "nissan skyline", "ferrari enzo"];
+
+for (let value of cars){
+    console.log(value);
+}
