22b0a3150dd53675993f2cd4bcf44c8071a8f1dc
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-09-25 14:11:10 -0300

    logic problem 3

---
+++ programmingLogic/class21/index.js
@@ -0,0 +1,6 @@
+// logic problem #2
+
+// write a function called ePaisagem which receives two arguments: width and height from numbers. Return true if the image is in the landscape mode
+
+const ePaisagem = (width, height) => width > height;
+console.log(ePaisagem(1080, 1900));
