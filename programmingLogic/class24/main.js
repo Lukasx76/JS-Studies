68c1c9f151d92e5b7b1ff88eded6dbd8bd2bcf3e
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-09-25 16:13:47 -0300

    learning about the finally block, which will be executed no matter what

---
+++ programmingLogic/class24/main.js
@@ -0,0 +1,24 @@
+function getHoursAndMinute(date) {
+    if (!date){
+        date = new Date()
+    }
+    if (!(date instanceof Date)) {
+        throw("Expecting a Date object");
+    }
+
+    return date.toLocaleTimeString("pt-BR", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12 : false
+    })
+}
+
+try {
+    console.log(getHoursAndMinute(new Date("10-18-2002 05:00")));
+    console.log(getHoursAndMinute(12));
+} catch(err) {
+    console.log(err);
+} finally {
+    console.log("Finishing the script...");
+}
