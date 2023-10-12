e6a024af8abd2bc578f194f36b21ef4b95549ef1
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-10-12 20:51:13 -0300

    almost there

--- functionsAdvancedModule/class9/assets/js/main.js
+++ functionsAdvancedModule/class9/assets/js/main.js
@@ -2,19 +2,23 @@
   let display = document.querySelector(".display td");
   const buttons = document.querySelector("tbody");
 
+  function displayStartsWithZero() {
+    if (display.innerText.startsWith("0")){
+      return true
+    } else {
+      return false
+    }
+  }
+
   function clearDisplay() {
     display.innerText = "";
   }
 
-  function deleteLastNumber(arr) {
-    const nodeSpan = display.querySelectorAll("span");
-    
-    const spanArr = []
-    for (let span of nodeSpan) {
-      let spanText = span.innerText;
-      spanArr.push(spanText);
+  function deleteLastNumber() {
+
+    if (display.hasChildNodes()){
+      display.removeChild(display.lastElementChild)
     }
-    console.log(spanArr)
   }
 
   function addOperatorsToDisplay(operator){
@@ -42,36 +46,78 @@
   }
 
   function addNumberToDisplay(number) {
-    if (number === "0") {
-      // check if the values present in the display don't start with 0
+    if (number === "0") {      
+      if (displayStartsWithZero()){
+      }
+      else {
+        display.innerHTML += `<span>0</span>`
+      }
+      
     }
 
     if (number === "1") {
-      display.innerHTML += `<span>1</span>`
+      if (displayStartsWithZero()){
+        display.innerHTML = `<span>1</span>`
+      } else {
+        display.innerHTML += `<span>1</span>`
+      }
     }
     if (number === "2") {
-      display.innerHTML += `<span>2</span>`
+      if (displayStartsWithZero()){
+        display.innerHTML = `<span>2</span>`
+      } else {
+        display.innerHTML += `<span>2</span>`
+      }
     }
     if (number === "3") {
-      display.innerHTML += `<span>3</span>`
+      if (displayStartsWithZero()){
+        display.innerHTML = `<span>3</span>`
+      } else {
+        display.innerHTML += `<span>3</span>`
+      }
     }
     if (number === "4") {
-      display.innerHTML += `<span>4</span>`
+      if (displayStartsWithZero()){
+        display.innerHTML = `<span>4</span>`
+      } else {
+        display.innerHTML += `<span>4</span>`
+      }
     }
     if (number === "5") {
-      display.innerHTML += `<span>5</span>`
+      if (displayStartsWithZero()){
+        display.innerHTML = `<span>5</span>`
+      } else {
+        display.innerHTML += `<span>5</span>`
+      }
     }
     if (number === "6") {
-      display.innerHTML += `<span>6</span>`
+      if (displayStartsWithZero()){
+        display.innerHTML = `<span>6</span>`
+      } else {
+        display.innerHTML += `<span>6</span>`
+      }
     }
     if (number === "7") {
-      display.innerHTML += `<span>7</span>`
+      if (displayStartsWithZero()){
+        display.innerHTML = `<span>7</span>`
+      } else {
+        display.innerHTML += `<span>7</span>`
+      }
     }
     if (number === "8") {
-      display.innerHTML += `<span>8</span>`
+      if (displayStartsWithZero()){
+        display.innerHTML = `<span>8</span>`
+      } else {
+        display.innerHTML += `<span>8</span>`
+      }
+      
     }
     if (number === "9") {
-      display.innerHTML += `<span>9</span>`
+      if (displayStartsWithZero()){
+        display.innerHTML = `<span>9</span>`
+      } else {
+        display.innerHTML += `<span>9</span>`
+      }
     }
   }
 
@@ -79,9 +125,12 @@
     const element = e.target
     addNumberToDisplay(element.innerText);
     addOperatorsToDisplay(element.innerText);
-    deleteLastNumber();
+    
 
-    console.log(typeof display.innerText);
+    if (element.innerText === "DEL") {
+      deleteLastNumber();
+    }
+
     if (element.innerText === "C") {
       clearDisplay();
     }
