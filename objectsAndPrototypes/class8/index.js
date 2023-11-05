f06636aafc2e72057a3313f0f84fc4853d58465a
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-11-05 13:30:55 -0300

    adding rules to check if the rest of the sum of the digits minus 11 are greater than 9. If it is assign the generated digit to 0

--- objectsAndPrototypes/class8/index.js
+++ objectsAndPrototypes/class8/index.js
@@ -14,8 +14,9 @@ segundo digito = 11 - (284 % 11) = 5
 */
 
 // 705484450 52
+// 97334339002
 
-const cpf = "70548445052";
+const cpf = "97334339002";
 let cpfCopy = cpf.slice(0, 9);
 let sum = 0;
 let multiplier = 10;
@@ -27,6 +28,7 @@ for (let i=0; i < cpfCopy.length; i++) {
 
     if (i === 8) {
         let firstDigit = 11 - (sum % 11);
+        if (firstDigit > 9) firstDigit = 0;
         cpfCopy += String(firstDigit)
         sum = 0;
         multiplier = 11;
@@ -38,6 +40,7 @@ for (let i=0; i < cpfCopy.length; i++) {
 
             if (j === 9) {
                 let secondDigit = 11 - (sum % 11);
+                if (secondDigit > 9) secondDigit = 0;
                 cpfCopy += String(secondDigit);
             }
         }
